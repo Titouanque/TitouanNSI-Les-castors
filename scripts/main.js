e43238c9f68b5e62d8b8cf28871f551f4568c3cc
Titouanque <96233155+Titouanque@users.noreply.github.com>
2022-01-05 13:26:29 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -43,9 +43,13 @@ function verifierQCM() {
     if (score == 5) {
         alert(`Exelent votre score est ${score}/5`)
     }
-    if ((score == 3) && (score == 4)) {
+    
+    if (score == 3) {
         alert(`Bien joué votre score est ${score}/5`)
     }
+	if (score == 4) {
+        alert(`Bien joué votre score est ${score}/5`)
+	}
     if (score < 3) {
         alert(`Votre score est ${score}/5 n'hésitez pas à réessayer le QCM`)
     }
